3c13d92f8e87ba0ba5dd21e9a852ec2155f2db7f
陌路拾荒 <xieqiudongv5@163.com>
2018-08-22 16:59:36 +0800

    1

--- MyApplication/app/src/main/java/com/example/pherson/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/pherson/myapplication/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.pherson.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
