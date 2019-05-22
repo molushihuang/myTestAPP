f86dfd15c1ac301e7e09d22382fe9c8c185de32f
陌路拾荒 <xieqiudongv5@163.com>
2019-05-22 09:43:49 +0800

    1

---
+++ MyApplication/app/src/main/java/com/example/pherson/myapplication/CommomUtils.java
@@ -0,0 +1,11 @@
+package com.example.pherson.myapplication;
+
+/**
+ * Created by 谢邱东 on 2019/3/1 14:23.
+ * NO bug
+ */
+
+public class CommomUtils {
+
+
+}
---
+++ MyApplication/app/src/test/java/com/example/pherson/myapplication/javatest/RandomTestTest.java
@@ -0,0 +1,14 @@
+package com.example.pherson.myapplication.javatest;
+
+import org.junit.Test;
+
+/**
+ * Created by 谢邱东 on 2019/2/14 14:27.
+ * NO bug
+ */
+public class RandomTestTest {
+    @Test
+    public void main() throws Exception {
+    }
+
+}
